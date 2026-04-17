52ec130ee4c34221dd9e8b9423dde7c56f0f9a0e
Ghanshyam Thacker <ghanshyam.thacker@simformsolutions.com>
2026-04-17 11:14:45 +0530

    changed index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.end("Hello from Docker this is second build");
+  res.end("kem chho vagadiya!!");
 });
 
 server.listen(3000, () => {
